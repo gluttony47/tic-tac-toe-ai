bce5e77b9dc20c75def70da716bda36bbe0c016e
monirul_islam_tahir <islam@mama-marketing.de>
2023-02-28 05:04:24 +0100

    created Grid dataclass

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 import enum
+import re
+from dataclasses import dataclass
+from functools import cached_property
+
 
 class Mark(str, enum.Enum):
     CROSS = "X"
@@ -9,8 +13,22 @@ class Mark(str, enum.Enum):
     def other(self)->Mark:
         return Mark.NAUGHT if self is Mark.CROSS else Mark.CROSS
         
+@dataclass(frozen=True)
+class Grid:
+    cells:str = " " * 9
     
-print(type(Mark.CROSS))
-print(Mark.CROSS.value)
-print(Mark.CROSS.value == 'X')
-print(Mark.CROSS == "X")
+    def __post_init__(self) -> None:
+        if not re.match(r"^[\sXO]{9}$", self.cells):
+            raise ValueError("Must contain 9 cells of: X, O, or space")
+    
+    @cached_property
+    def x_count(self) -> int:
+        return self.cells.count("X")
+    @cached_property
+    def o_count(self) -> int:
+        return self.cells.count("O")
+    @cached_property
+    def empty_count(self) -> int:
+        return self.cells.count(" ")
+
+grid = Grid("XXOOXXOOX")  
